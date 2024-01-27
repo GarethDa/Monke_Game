7a37872a578260c7e0e51c1cd7b1182bd02036a3
Monkey Maker <>
2024-01-27 00:31:05 -0500

    got throwing working

--- PickUpItem.gd
+++ PickUpItem.gd
@@ -1,7 +1,9 @@
 extends Area3D
 
 @export var player : Node3D
+@export var heldItemTransform : RemoteTransform3D
 var pickedUpItem : Node3D
+
 
 		
 func _input(event: InputEvent) -> void:
@@ -9,8 +11,9 @@ func _input(event: InputEvent) -> void:
 		if not pickedUpItem:
 			return
 		
-		
-		pickedUpItem.apply_central_impulse(player.mesh.transform.basis.z*20)
+		heldItemTransform.remote_path = ""
+		pickedUpItem.set_freeze_enabled(false)
+		pickedUpItem.apply_central_impulse((player.mesh.transform.basis.z*20) + (player.mesh.transform.basis.y*3))
 		print("THROW")
 		pass
 		
@@ -19,6 +22,8 @@ func setPickedUpItem(body):
 		return
 	if not pickedUpItem:
 		pickedUpItem = body
+		heldItemTransform.remote_path = pickedUpItem.get_path()
+		pickedUpItem.set_freeze_enabled(true)
 
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	setPickedUpItem(body)
